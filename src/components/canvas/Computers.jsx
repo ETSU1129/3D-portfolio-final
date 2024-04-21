2f10dbde84d1c62e654cc75526e19edf1acab39b
ETSU1129 <322D15029@edu.thu.ac.jp>
2024-04-21 19:25:47 +0900

    コンピューターコンポーネントのキャンバスのスタイルを修正

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -58,6 +58,7 @@ const ComputersCanvas = () => {
             dpr={[1, 2]}
             camera={{ position: [20, 3, 5], fov: 25 }}
             gl={{ preserveDrawingBuffer: true }}
+            style={{pointerEvents: 'none'}}
             >
                 
             <Suspense fallback={<CanvasLoader />}>
