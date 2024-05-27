7ffb5102b0e1ca317973cc540540e30a31990543
Etsu <--ui185311@gmail.com>
2024-05-27 20:05:45 +0900

    コンポーネントのスタイル修正とモデル修正

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -50,7 +50,7 @@ const About = () => {
         I'm a skilled software developer with experience in TypeScript and
         JavaScript, and expertise in frameworks like React, Node.js, and
         Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
+        create efficient, scalable, and user-friendly solutions
         real-world problems. 
       </motion.p>
 
